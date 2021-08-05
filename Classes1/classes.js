21236f7a3642c37a3c7328ec45476fa0a56283cd
Ywette <ywette.pro@gmail.com>
2021-08-05 12:49:32 +0300

    excercises js oop classes
    static, public, private methods

---
+++ Classes1/classes.js
@@ -0,0 +1,53 @@
+class Car {
+    constructor(make ,model, year, color, engineType){
+        this.make = make,
+        this.model = model,
+        this.year = year,
+        this.color = color;
+        this.engineType = engineType
+    }
+    getDetails(){            
+        return this;
+       }    
+}
+
+class CarTwo extends Car {
+    constructor(make ,model, year, color, engineType, odometer){
+       super(make ,model, year, color, engineType);
+            this.odometer = odometer
+    }
+
+    getDetails(){            
+        return this;
+       } 
+
+    isUsed(){
+            if (odometer > 2000){
+                return "(Adult)";
+            }else {
+                return odometer
+            }
+       }        
+}
+
+
+class CarThree extends Car {
+    //ecma6 - private variables
+        #make;
+        #model;
+        #year;
+        #color;
+        #engineType;
+
+    getPrivateValues(){
+        console.log(this);
+        return this;
+    }
+}
+
+
+class CarFour extends Car {
+    static getDetails(){
+        return this;
+    }
+}
---
+++ Classes1/main.js
@@ -0,0 +1,51 @@
+
+let car1 = new Car('Audi', 'Avant', '1998', 'black', 'gas');
+let car2 = new CarTwo('BMW', 'bai', 'bum', 'digi', 'bai', 5500);
+let car3 = new CarTwo('3 private data', '3 private data', '3 private data', '3 private data', '3private data');
+let car4 = new CarTwo('4 static method', '4 static method', '4 static method', '4 static method', '4 static method');
+
+
+function showData(pic){
+        
+    switch (pic.id){
+        case "one":
+            LoadCarData(car1);
+            break;
+
+        case "two":                
+            // console.log(car2.isUsed())
+            LoadCarData(car2);
+            document.getElementById("odometerId").hidden=false;
+            document.getElementById("odometer").innerHTML=car2.odometer, isUsed();
+
+            break;   
+
+        case "three":                
+            LoadCarData(car3);
+            car3.getDetails();
+
+            break;   
+
+        case "four":                
+            LoadCarData(car4);
+            CarFour.getDetails(car4);
+            break;  
+
+
+        default:
+            console.log('DEFAULTTTTTTT')   
+    }
+}
+
+function LoadCarData(c) {
+    document.getElementById('make').innerHTML=c.make;
+    document.getElementById('model').innerHTML=c.model;
+    document.getElementById('year').innerHTML=c.year;
+    document.getElementById('color').innerHTML=c.color;
+    document.getElementById('engineType').innerHTML=c.engineType;   
+    document.getElementById("odometerId").hidden=true;
+}
+
+
+
+
